ed5dfd8b537d84396cd7a4d72651d7829b47ca1a
Iris Yoon <irishryoon@gmail.com>
2020-12-18 13:41:30 -0500

    started streamlit app

---
+++ dashboard/magistrate.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title('Breakdown by Magistrate')
+    st.write('In progress')
---
+++ dashboard/neighborhood.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title('Breakdown by Neighborhood')
+    st.write('In progress')
