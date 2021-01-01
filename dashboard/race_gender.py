17d9f2886328c7359574bc0962fced72e4d19a03
SW Quader <syedawquader@gmail.com>
2021-01-01 15:55:04 -0500

    Add figure for race_gender page

--- dashboard/race_gender.py
+++ dashboard/race_gender.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 def app():
     st.title('Breakdown by Race & Gender')
     st.write('In progress')
+    
+    # was bail paid?
+    image = Image.open('figures/bail_paid_race.png')
+    st.image(image)
