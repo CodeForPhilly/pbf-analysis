43003fc26c905193eed861d2e8d1ff6b53c51b04
Iris Yoon <irishryoon@gmail.com>
2021-01-02 16:28:14 -0500

    edit magistrate page of dashboard

--- dashboard/magistrate.py
+++ dashboard/magistrate.py
@@ -2,43 +2,65 @@ import streamlit as st
 from PIL import Image
 
 def app():
+
     st.title('Breakdown by Magistrate')
-    st.write('In progress')
+    st.write('This section provides a summary of how bail type and bail amount depend on the person setting the bail (hereby referred to as magistrate).')
 
     ### 2020 Summary plots
 
-    # !!!!! TODO !!!!!
     # Explain selection of magistrates (Those who handled more than 500 cases)
     # Some timeline (Many were involved consistently throughout the year. Some were seasonal)
     # Explain 'Others': Those who set fewer than 500 cases. Total number of those labeled as "Others"
 
     # number of cases
-    st.header("Year End Summary by Magistrate")
+    st.header("1. Year-End Summary by Magistrate")
     st.subheader("Number of cases handled by each magistrate")
+
+    _, col, _ = st.beta_columns([1,2,1])
     image = Image.open('figures/magistrate_case_count.png')
-    st.image(image)
+    col.image(image)
+    st.write("In the year 2020, bail was set by 625 different people. We provide a summary of the bail type and bail amount for cases handled by the 9 magistrates who handled more than 300 cases in year 2020.")
+
     # bail type
     st.subheader("Percentage of bail type for each magistrate")
     image = Image.open('figures/magistrate_type_summary.png')
     st.image(image, use_column_width=True)
+    st.write("The above pie chart shows the percentage of bail types set by each magistrate. \
+            While there isn't a huge variability across magistrates, we can see that E-filing judges set fewer monetary bail than other magistrates.")
+
     # bail amount
     st.subheader("Monetary bail amount set by each magistrate")
     image = Image.open('figures/magistrate_amount_summary.png')
-    st.image(image)
+    _, col, _ = st.beta_columns([1, 5, 1])
+    col.image(image, use_column_width = True)
+    st.write("The above box plot compares the monetary bail amount set by different magistrates. \
+            For each magistrate, the vertical line in the colored box represents the median bail amount set by that magistrate. \
+            The colored boxes represent the 25% to 75% range of the bail amount set by the magistrate. The dots represent outliers.")
+    st.write("On average (median), the bail amount set by Connor, Bernard, and Rigmaiden-DeLeon ($50k) is higher than bail amount set by others ($25k). \
+            Moreover, Connor and Bernard seem to have set a wider range of bail amounts than other magistrates. ")
+    
 
     ### Comparison controling for offense types 
-    st.header("Analysis while controling for offense types")
-
-    # !!!!! TODO !!!!!
-    # Explain the matched analysis. How many cases are sampled for the analysis
-    # Explain the selection of 5 magistrates (those who handled more than 1000 cases)
-    # Write interpretations
+    st.header("2. Analysis while controlling for offense types")
+    st.write("While the above figures provide a useful year-end summary, they do not provide a fair comparison of the magistrates. \
+            In particular, the differences among magistrates may stem from the fact that some magistrates may have handled more cases with more severe charges.")
+            
+    st.write("We compared the bail type and bail amount set by the magistrates while controlling for the difference in the charges. \
+            We conducted a matched study where we sampled cases with the same charges that were handled by five different magistrates (Bernard, Rigmaiden-DeLeon, Stack, E-Filing Judge, and O'Brien). \
+            We performed the matched study on only the five magistrates that handled more than 1000 cases in year 2020. ")
+            
+    st.write("The following results were obtained from the 3490 cases (698 per magistrate) that were sampled. Ideally, there shouldn't be any noticable difference across magistrates.")
     
     # bail type
     st.subheader("Percentage of bail type for each magistrate")
     image = Image.open('figures/magistrate_matched_type.png')
     st.image(image, use_column_width=True)
+    st.write("When we control for the offense types, magistrates set monetary bail to similar number of cases.")
+
     # bail amount
     st.subheader("Monetary bail amount set by each magistrate")
+    _, col, _ = st.beta_columns([1, 5, 1])
     image = Image.open('figures/magistrate_matched_amount.png')
-    st.image(image)
+    col.image(image, use_column_width = True)
+    st.write("Even when we control for the offense types, we see a difference in the monetary bail amount across magistrates.\
+            In particular, Bernard and Rigmaiden-DeLeon set higher bail amounts than Stack, E-filing Judge, and O'Brien.")
