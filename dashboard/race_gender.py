e33e19fa812f71006ac59e47a5386d377db10b0c
notchia <lucia.korpas@gmail.com>
2021-01-21 12:05:40 -0500

    add commentary on matched study

--- dashboard/race_gender.py
+++ dashboard/race_gender.py
@@ -22,12 +22,10 @@ def app():
 
     st.subheader("Bail amount frequency")
     st.write("When monetary bail is set, White defendants were more frequently assigned a bail amount between $10,000 and $25,000, and Black defendants were more frequently assigned a bail amount between $100,000 and $500,000.")
-    st.image(Image.open('figures/bail_set_bin.png'), width=imgWidth)
+    st.image(Image.open('figures/race_aggregate_set.png'), width=imgWidth)
     
     st.subheader("Bail posted frequency")
-    
     st.image(Image.open('figures/bail_paid_race.png'), width=imgWidth)
-    
     st.image(Image.open('figures/bail_paid_race_1k to 5k.png'), width=imgWidth)
     st.image(Image.open('figures/bail_paid_race_5k to 10k.png'), width=imgWidth)
     st.image(Image.open('figures/bail_paid_race_10k to 25k.png'), width=imgWidth)
@@ -43,7 +41,12 @@ def app():
     # Matched analysis
     # ----------------------------------------
     st.header("Controlling for offense types")
-
+    st.write(
+    """While the above figures provide a useful year-end summary, they include variation in bail types and amounts that may be attributed to factors other than race, such as offense types. 
+    
+To control for offense types, we conducted a matched study where we sampled cases with identical lists of charges from cases with White and Black defendants. The following results were obtained from the 10593 cases (5115 for Black defendants, 5112 for White defendants) that were sampled. """
+    )
+        
     st.image(Image.open('figures/race_matched_type.png'), width=int(1.5*imgWidth))    
     st.image(Image.open('figures/race_matched_set.png'), width=imgWidth)    
 
