baa6c4a0f750b37759930784e9c267f55d1a91b0
notchia <lucia.korpas@gmail.com>
2021-03-19 01:43:11 -0400

    update dashboard.ipynb to use pltformat.py

--- analysis/pltformat.py
+++ analysis/pltformat.py
@@ -6,6 +6,16 @@ Color scheme is based on PBF website, using https://www.colorhexa.com/
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+def get_color_dictionary():
+    colorDict = {'main':'#324dba', #title_blue
+                 'dark':'#263a8c', #darker_blue
+                 'light':'#3291ba', #sky_blue
+                 'complement':'#32ba9f' #green
+                 }
+    return colorDict
+
+
 plt.style.use('ggplot')
 
 sns.set_theme(rc={
@@ -16,10 +26,4 @@ sns.set_theme(rc={
     'axes.spines.right': False, 
     'axes.spines.top': False, 
 })
-
-# Currently unused, I think?
-title_blue = '#324dba'
-darker_blue = '#263a8c'
-sky_blue = '#3291ba'
-green = '#32ba9f'
 
