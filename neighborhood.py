3ad895b1da541d3961673f8b2fee42dedf91f41e
notchia <lucia.korpas@gmail.com>
2021-05-12 13:27:33 -0400

    merge price and neighborhood pages

--- neighborhood.py
+++ neighborhood.py
@@ -44,15 +44,162 @@ def preprocess_acs():
     
     return acs_df
 
+@st.cache()
+def load_data_paid():
+    df_month = pd.read_csv('data/cleaned/app_bail_paid.csv')
+    return df_month
+
+
 def app():
-    # year-end summary
+    # ----------------------------------
+    #  Year-end summary (included on every page)
+    # ----------------------------------
     fig = plot_year_summary()
     f_year = go.FigureWidget(fig)
     st.plotly_chart(f_year)
 
-    minCount = 10 #100
+    # ----------------------------------
+    #  Introduction
+    # ----------------------------------    
     
-    st.title('Breakdown by Neighborhood')
+    st.title('Total price paid by Philadelphians')
+    
+    st.write("""When monetary bail is set, a defendent is detained in jail while awaiting trial unless they pay a portion (typically 10%) of the bail amount set.
+Depending on how much bail was set, the amount that a defendant must pay can be thousands of dollars. 
+This can be financially difficult for many Philadelphia families. """) 
+
+    # ----------------------------------
+    #  Interactive figure: Total price paid
+    # ----------------------------------       
+    
+    st.subheader('How much have Philadelphians paid in bail?')
+
+    st.write("In 2020, Philadelphians paid **tens of millions of dollars** in bail to be released from jail while they await their trials.")
+    
+    minCount = 10 #100
+
+    # prepare data
+    df_month = load_data_paid()
+    month_data = df_month['bail_paid']
+
+    df_year = df_month.groupby(['bail_year'])['bail_paid'].sum()
+    bail_paid_2020 = df_year[2020]
+    bail_paid_2021 = df_year[2021]
+
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+    # Plot figure
+    fig = go.Figure()
+
+    ### Add traces for yearly summary
+    # 2020
+    fig.add_trace(go.Bar(
+    x = [0],
+    y = [bail_paid_2020],
+    orientation = "v",
+    text = ["$"+f'{bail_paid_2020:,.0f}'],
+    textposition = "inside",
+    hoverinfo = "text",
+    hovertext = ["2020 <br>$"+f'{bail_paid_2020:,.0f}'],
+    showlegend = False
+    ))
+
+    # 2021
+    fig.add_trace(go.Bar(
+        x = [1],
+        y = [bail_paid_2021],
+        orientation = "v",
+        text = ["$"+f'{bail_paid_2021:,.0f}'],
+        textposition = "inside",
+        hoverinfo = "text",
+        hovertext = ["2021 <br>$"+f'{bail_paid_2021:,.0f}'],
+        showlegend = False
+
+        ))
+
+    ### add traces for monthly summary
+    # 2020 data
+    hovertext_2020 = [m + " 2020 <br>" + "$"+f'{v:,.0f}'  
+                    for (m,v) in zip(months, month_data[:12])]
+    fig.add_trace(go.Bar(
+        y = month_data[:12],
+        name = "2020",
+        hoverinfo = "text",
+        hovertext = hovertext_2020,
+        visible = False
+        ))
+
+    # 2021 data
+    hovertext_2021 = [m + " 2021 <br>" + "$"+f'{v:,.0f}'
+                    for (m,v) in zip(months,month_data[12:])]
+    fig.add_trace(go.Bar(
+        y = month_data[12:],
+        name = "2021",
+        hoverinfo = "text",
+        hovertext = hovertext_2021,
+        visible = False   
+        ))
+
+
+    fig.update_layout(
+        margin={'t':25},
+        xaxis_title="Year",
+        yaxis_title="Bail amount paid ($)",
+        xaxis_tickvals = [0, 1],
+        xaxis_ticktext = ["2020","2021 YTD"]
+    )
+
+    fig.update_layout(
+        annotations=[
+            dict(text="Select data to view:",
+                 x=-0.5,
+                 y=1.00,
+                 xref="paper",
+                 yref="paper",
+                 align="left",
+                 showarrow=False
+                )
+        ]
+    ) 
+
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                active=0,
+                x = -0.5,
+                xanchor = 'left',
+                y = 0.92,
+                yanchor = 'top',
+                buttons=list([
+                    dict(label="by year",
+                        method="update",
+                        args=[{"visible": [True, True, False, False]},
+                            {'xaxis': {'title': 'year',
+                                        'tickvals' : [0,1],
+                                        'ticktext' : ["2020", "2021 YTD"]
+                            }}]),
+                    dict(label="by month",
+                        method="update",
+                        args=[{"visible": [False, False, True, True]},
+                            {'xaxis': {'title': 'month',
+                                        'tickvals' : list(range(12)),
+                                        'ticktext' : months
+                                        }}
+                            ])
+                        
+                ]),
+            )
+            
+        ])
+
+    f2 = go.FigureWidget(fig)
+    st.plotly_chart(f2)
+
+    # ----------------------------------
+    #  Interactive figure: Neighborhood maps
+    # ----------------------------------       
+    
+    st.subheader('Breakdown by Neighborhood')
     st.write('This section provides an interactive breakdown of case counts, total amounts of bail set and paid, and bail payment rate by Philadelphia zip code, in tandem with income, poverty, and unemployment data from the American Community Survey (ACS) collected by the U.S. Census Bureau.')
     
     st.write('The plot on the left shows bail-related summary for each zipcode, while the plot on the right shows income, poverty level, and unemployment rate for each zipcode. One can see that the neighborhoods that are strongly affected by the bail system often correspond to neighborhoods with low income, high unemployment, and high poverty.')
